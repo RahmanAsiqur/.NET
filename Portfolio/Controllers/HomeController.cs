661293209a5c78b9d591be81e4063ecc0e2b9a00
asifu <asifu@HIGHMAN>
2023-09-22 21:39:51 +0600

    Add project files.

---
+++ Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            ViewBag.Message1 = "HSC";
+            ViewBag.Message2= "Institution : Patuakhali GOVT College";
+            ViewBag.Message3 = "Year : 2019";
+            ViewBag.Message4 = "Grp : Science";
+            ViewBag.Message5 = "Result : 4.31 ";
+
+
+            return View();
+        }
+
+        public ActionResult References()
+        {
+          return View();
+        }
+    }
+}
